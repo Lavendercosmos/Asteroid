268640260ba60da62562cdd4cc5a895cd16450be
vivi <muanpor@gmail.com>
2024-10-26 00:49:24 +0700

    add log4j2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module se233.asteroid {
+module se.asteroid {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-
+    requires org.apache.logging.log4j;
 
     opens se233.asteroid to javafx.fxml;
     exports se233.asteroid;
